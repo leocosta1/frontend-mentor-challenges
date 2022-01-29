850c8176c11ab33dfbf94d3ca8dbc021037d4e1f
Leonardo Costa <leorizzocosta@hotmail.com>
2022-01-29 18:46:44 -0300

    Finished basic file sctructure

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const techsList = {
+  1: 'HTML',
+  2: 'CSS',
+  3: 'JS',
+  4: 'API',
+};
+
+const difficultiesList = {
+  1: 'Newbie',
+  2: 'Junior',
+  3: 'Intermediate',
+  4: 'Advanced',
+  5: 'Guru',
+};
