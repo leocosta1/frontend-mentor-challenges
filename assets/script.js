3ee4fe312a4f6870a8174636b783def0e840c91e
Leonardo Costa <leorizzocosta@hotmail.com>
2022-01-31 16:44:35 -0300

    Finished card component

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,45 @@ const difficultiesList = {
   4: 'Advanced',
   5: 'Guru',
 };
+
+const projectsList = [];
+
+projectsList.forEach(({ title, img, url, techs, difficulty }) => {
+  const cardsContainer = document.querySelector('.main__cards');
+
+  const card = document.createElement('li');
+  card.classList.add('card');
+  card.innerHTML = `
+    <a href="${url}" target="_blank" class="card__content">
+      <img
+        src="${img}"
+        alt="${title}"
+        class="card__image"
+      />
+
+      <div class="card__overlay">
+        <strong class="card__title">${title}</strong>
+
+        <div class="card__techs">
+        </div>
+
+        <span class="card__badge card__badge--${difficulty}">
+          <span class="card__num">${difficulty}</span>
+          <span class="card__difficulty">${difficultiesList[difficulty]}</span>
+        </span>
+      </div>
+    </a>
+  `;
+
+  techs.forEach((tech) => {
+    const techsContainer = card.querySelector('.card__techs');
+
+    const techEl = document.createElement('span');
+    techEl.classList.add('card__tech', `card__tech--${tech}`);
+    techEl.innerHTML = techsList[tech];
+
+    techsContainer.appendChild(techEl);
+  });
+
+  cardsContainer.appendChild(card);
+});
